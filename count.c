c20140846d2a00ba944706474e447b3074ccdddb
AishSaag <38650933+AishSaag@users.noreply.github.com>
2018-08-04 06:35:46 +0000

    Create count.c

---
+++ count.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int b[20],N,K,i,sum=0
+  scanf("%d",&N);
+  scanf(%d",&K);
+  for(i=0;i<N;i++)
+  {
+    scanf("%d",&b[i]);
+  }
+  for(i=0;i<K;i++)
+  {
+    sum=sum+b[i]
+  }
+  printf("%d",sum);
+  return 0;
+}
